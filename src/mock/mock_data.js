691458e2433705253f054f7e62bb0de57525bb4b
mjnproton <51047235+mjnproton@users.noreply.github.com>
2025-09-11 20:25:25 +0800

    chore: 更新导航数据 - 2025/9/11 20:25:25

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -117,6 +117,13 @@ export const mockData = {
           "url": "https://copilot.microsoft.com/chats/Thpw5BGzCtoi7D9YEBatH",
           "description": "",
           "icon": ""
+        },
+        {
+          "id": "site-1757593520572",
+          "name": "apen ai studio",
+          "url": "https://platform.openai.com/chat",
+          "description": "",
+          "icon": ""
         }
       ]
     },
@@ -174,20 +181,6 @@ export const mockData = {
           "url": "https://www.pasyun.com/",
           "description": "ipv6机器",
           "icon": "/sitelogo/www.pasyun.com.ico"
-        },
-        {
-          "id": "site-1757593460826",
-          "name": "赵云",
-          "url": "https://us-west-1.run.claw.cloud/signin",
-          "description": "",
-          "icon": ""
-        },
-        {
-          "id": "site-1757593475511",
-          "name": "拥抱脸",
-          "url": "https://huggingface.co/",
-          "description": "",
-          "icon": ""
         }
       ]
     },
@@ -300,20 +293,6 @@ export const mockData = {
           "id": "site-1757593272844",
           "name": "avast密码",
           "url": "https://www.avast.com/zh-cn/random-password-generator#pc",
-          "description": "",
-          "icon": ""
-        },
-        {
-          "id": "site-1757593359663",
-          "name": "香港地址生成",
-          "url": "https://www.meiguodizhi.com/hk-address/hot-city-jiu-long-cheng-qu",
-          "description": "",
-          "icon": ""
-        },
-        {
-          "id": "site-1757593374006",
-          "name": "在线地址生成",
-          "url": "https://ratenn.com/",
           "description": "",
           "icon": ""
         }
