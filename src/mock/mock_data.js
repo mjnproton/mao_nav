e6afd5c6d2846dd9b30c7608e51134491eeccc9f
mjnproton <51047235+mjnproton@users.noreply.github.com>
2025-09-22 22:09:32 +0800

    chore: 更新导航数据 - 2025/9/22 22:09:29

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -670,13 +670,6 @@ export const mockData = {
           "url": "https://www.dongaigc.com/nav/ai-video",
           "description": "",
           "icon": ""
-        },
-        {
-          "id": "site-1758549269318",
-          "name": "24k导航",
-          "url": "https://24kdh.vip/",
-          "description": "",
-          "icon": ""
         }
       ]
     },
